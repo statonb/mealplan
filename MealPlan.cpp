6e072a486f2972dc0372b2c79891ef1a3743deda
Brian Staton <brian@carrarasoftware.com>
2024-07-03 17:54:15 -0400

    initial commit

---
+++ MealPlan.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <termios.h>
+#include <vector>
+#include <string>
+
+const char *menuFileName = "/mnt/omv1share1/Workspace/MealPlan/menu.txt";
+const char *recentSelectionsFileName = "/mnt/omv1share1/Workspace/MealPlan/recentSelections.txt";
+const size_t DEFAULT_MAX_NUM_RECENT_SELECTIONS = 14;
+
+bool isInVector(std::vector<int> *pV, int target)
+{
+    std::vector<int>::const_iterator itV = pV->begin();
+    while (itV != pV->end())
+    {
+        if (target == *itV)
+        {
+            return true;
+        }
+        itV++;
+    }
+    return false;
+}
+int main(int argc, char *argv[])
+{
+    int selection;
+    FILE *fpMenu;
+    FILE *fpRecentSelections;
+    char *cp;
+    char tempLine[256];
+    std::vector<std::string> theMenu;
+    std::vector<int> recentSelections;
+    std::vector<int>::const_iterator itRecentSelections;
+    bool exitFlag = false;
+    bool acceptFlag = false;
+    char acceptYN;
+    size_t maxNumRecentSelections = DEFAULT_MAX_NUM_RECENT_SELECTIONS;
+    struct termios oldSettings;
+    struct termios newSettings;
+
+    fpMenu = fopen(menuFileName, "r");
+    if ((FILE *)(NULL) == fpMenu)
+    {
+        fprintf(stderr, "Can't open menu file.\n");
+        exit(1);
+    }
+    while (fgets(tempLine, sizeof(tempLine), fpMenu))
+    {
+        cp = strrchr(tempLine, '\n');
+        if ((char *)(NULL) != cp)
+        {
+            *cp = '\0';
+        }
+        theMenu.push_back(tempLine);
+    }
+    fclose(fpMenu);
+
+    fpRecentSelections = fopen(recentSelectionsFileName, "r");
+    if ((FILE *)(NULL) != fpRecentSelections)
+    {
+        while(fgets(tempLine, sizeof(tempLine), fpRecentSelections))
+        {
+            selection = (int)strtol(tempLine, NULL, 10);
+            recentSelections.push_back(selection);
+        }
+        fclose(fpRecentSelections);
+    }
+
+    tcgetattr(STDIN_FILENO, &oldSettings);
+    newSettings = oldSettings;
+    newSettings.c_lflag &= (~ICANON);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
+
+    srand(time(NULL));
+
+    while (false == acceptFlag)
+    {
+        exitFlag = false;
+        while (false == exitFlag)
+        {
+            selection = rand() % theMenu.size();
+            if (false == isInVector(&recentSelections, selection))
+            {
+                std::cout << '\n' << selection << ' ' << theMenu[selection] << '\n';
+                exitFlag = true;
+            }
+        }
+        printf("Accept? <Y/N>: ");
+        acceptYN = (char)getchar();
+        if  (   ('Y' == acceptYN)
+             || ('y' == acceptYN)
+            )
+        {
+            acceptFlag = true;
+        }
+        putchar('\n');
+    }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
+
+    fpRecentSelections = fopen(recentSelectionsFileName, "w");
+    if ((FILE *)(NULL) == fpRecentSelections)
+    {
+        fprintf(stderr, "Can't open recent selections file.\n");
+        exit(2);
+    }
+    itRecentSelections = recentSelections.begin();
+    if  (   (itRecentSelections != recentSelections.end())
+         && (recentSelections.size() >= maxNumRecentSelections)
+        )
+    {
+        itRecentSelections++;
+    }
+    while (itRecentSelections != recentSelections.end())
+    {
+        fprintf(fpRecentSelections, "%d\n", *itRecentSelections);
+        itRecentSelections++;
+    }
+    fprintf(fpRecentSelections, "%d\n", selection);
+    fclose(fpRecentSelections);
+
+    return 0;
+}
